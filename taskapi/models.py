19f4f9ba1d4659ff6696e52a32044de5aac75981
Luz Angélic Jiménez Rozo <langelicajr@gmail.com>
2019-08-07 20:34:27 +0000

    4. Define models - userTasks

--- taskapi/models.py
+++ taskapi/models.py
@@ -6,3 +6,16 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserTasks(models.Model):
+    TASK_STATES = (
+        ('TO_DO', 'To do'),
+        ('L', 'Large'),
+    )
+    description = models.CharField(max_length=300, blank=False)
+    state = models.CharField(max_length=300, blank=False, choices=TASK_STATES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
