08dfbbf75fdc0881dbb67cf7ab54a89bb9356d09
Luz Angelica Jiménez Rozo <luz.jimenez.consultant@sothebys.com>
2019-08-08 22:02:21 -0500

    Serializar enhancement

--- taskapi/serializers.py
+++ taskapi/serializers.py
@@ -4,13 +4,16 @@ from .models import User
 from .models import UserTasks
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ('id', 'name', 'username')
 
 
-class UserTasksSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = UserTasks
-        fields = ('id', 'description', 'state', 'user')
+class UserTasksSerializer(serializers.ModelSerializer):
+        user = UserSerializer(read_only=True)
+
+        class Meta:
+            model = UserTasks
+            fields = ('id', 'description', 'state', 'user')
