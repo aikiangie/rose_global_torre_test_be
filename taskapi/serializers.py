f12ce9283f168dea8cd203ff4cd95ecc9f04de46
Luz Angelica Jiménez Rozo <luz.jimenez.consultant@sothebys.com>
2019-08-09 09:06:13 -0500

    UserId fix

--- taskapi/serializers.py
+++ taskapi/serializers.py
@@ -13,7 +13,8 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserTasksSerializer(serializers.ModelSerializer):
         user = UserSerializer(read_only=True)
+        userId = serializers.PrimaryKeyRelatedField(write_only=True, queryset=User.objects.all(), source='user')
 
         class Meta:
             model = UserTasks
-            fields = ('id', 'description', 'state', 'user')
+            fields = ('id', 'description', 'state', 'user', 'userId')
